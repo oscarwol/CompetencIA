3d795215bde7331511dc6d4553504419c3d2c45d
oscarwol <omorales@wol.group>
2023-09-05 10:56:40 -0600

    Fixed Issues

--- app/app.py
+++ app/app.py
@@ -13,9 +13,9 @@ from werkzeug.utils import secure_filename
 import os
 from sqlalchemy.orm import Session
 from sqlalchemy import func
-
+from io import BytesIO
 import pathlib
-from openpyxl import Workbook
+import xlsxwriter
 
 
 CURRENT_PATH = pathlib.Path(__file__).parent.resolve()
@@ -23,8 +23,8 @@ CURRENT_PATH = pathlib.Path(__file__).parent.resolve()
 host = "/backend"
 app = Flask(__name__)
 CORS(app)
-url = "mysql+pymysql://devus:d3vc0mp7.23!@cptwol.clurs6kstakf.us-west-1.rds.amazonaws.com/Pruebas"
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL')
+url = "mysql+pymysql://devus:d3vc0mp7.23!@cptwol.clurs6kstakf.us-west-1.rds.amazonaws.com/COMPETENCIA"
+app.config["SQLALCHEMY_DATABASE_URI"] = url     #os.environ.get('DATABASE_URL')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = os.environ.get('SECRET_KEY')
 
@@ -34,7 +34,7 @@ base = db.Model.metadata.reflect(db.engine)
 
 session = Session(db.engine, future=True)
 
-""" 
+"""
 Modelos y Esquemas
 """
 
@@ -132,14 +132,11 @@ etl_scehma = ETL_Diario_Schema()
 etl_scehmas = ETL_Diario_Schema(many=True)
 
 
-""" 
-Funciones de Usuarios (Login, register)
-"""
-
 
 @app.route(host + "/totales", methods=["GET"])
 def get_totales():
     query_columns = [
+        func.count(ETL_Diario.CLIENTE.distinct()),
         func.count(ETL_Diario.MEDIO.distinct()),
         func.count(ETL_Diario.MARCA.distinct()),
         func.count(ETL_Diario.CATEGORIA.distinct()),
@@ -148,10 +145,11 @@ def get_totales():
         func.count(ETL_Diario.VERSION.distinct())
     ]
 
-    medios, marcas, categorias, productos, anunciantes, versiones = db.session.query(
+    clientes, medios, marcas, categorias, productos, anunciantes, versiones = db.session.query(
         *query_columns).first()
 
     datas = {
+        "clientes": clientes,
         "marcas": marcas,
         "categorias": categorias,
         "productos": productos,
@@ -169,7 +167,6 @@ def get_totales():
 @app.route(host + "/", methods=["GET"])
 def get_index():
         return jsonify("Sistema funcionando correctamente en el puerto 7000"), 200
-
 
 
 @app.route(host + "/categorias", methods=["GET"])
@@ -222,6 +219,15 @@ def get_marcas():
     marcas = [row[0] for row in result]
     if marcas:
         return jsonify(marcas), 200
+    return "No hay resultados", 404
+
+
+@app.route(host + "/clientes", methods=["GET"])
+def get_clientes():
+    result = db.session.query(ETL_Diario.CLIENTE.distinct()).all()
+    clientes = [row[0] for row in result]
+    if clientes:
+        return jsonify(clientes), 200
     return "No hay resultados", 404
 
 
@@ -317,6 +323,7 @@ def get_etl_diario():
 def generar_excel():
     data = request.form
     # Obtener los datos del cuerpo del POST
+    cliente = data.get("cliente", "")
     categoria = data.get("categoria", "")
     anunciante = data.get("anunciante", "")
     marca = data.get("marca", "")
@@ -325,9 +332,21 @@ def generar_excel():
     tipomedio = data.get("tipodemedio", "")
     medio = data.get("medio", "")
     fecha_inicio = data.get("fecha_inicio", "")
-    fecha_fin = data.get("fecha_fin", "")
 
     etl_diario_query = ETL_Diario.query
+
+    if fecha_inicio:
+        fecha_inicio = datetime.strptime(fecha_inicio, "%Y-%m-%d").date()
+        fecha_mes = fecha_inicio.month
+        fecha_anio = fecha_inicio.year
+        etl_diario_query = etl_diario_query.filter(
+            (ETL_Diario.MES == fecha_mes) &
+            (ETL_Diario.ANIO == fecha_anio)
+        )
+        
+    if cliente:
+        etl_diario_query = etl_diario_query.filter(
+            ETL_Diario.CLIENTE == cliente)        
 
     if categoria:
         etl_diario_query = etl_diario_query.filter(
@@ -355,47 +374,40 @@ def generar_excel():
     if medio:
         etl_diario_query = etl_diario_query.filter(ETL_Diario.MEDIO == medio)
 
-    if fecha_inicio and fecha_fin:
-        fecha_inicio = datetime.strptime(fecha_inicio, "%Y-%m-%d").date()
-        fecha_fin = datetime.strptime(fecha_fin, "%Y-%m-%d").date()
-        print(fecha_inicio, fecha_fin)
-        etl_diario_query = etl_diario_query.filter(
-            ETL_Diario.FECHA.between(fecha_inicio, fecha_fin))
-
     results = etl_diario_query.all()  # Obtener todos los resultados
 
+
+
     if results:
-        # Crear el archivo Excel y agregar los resultados
-        wb = Workbook()
-        ws = wb.active
+        headers = ['ADSERVER', 'AGENCIA', 'ALTO', 'ANCHO', 'ANIO', 'ANUNCIANTE', 'AREA', 'AVISO', 'CAMPANIA', 'CATEGORIA',
+        'CLIENTE', 'DateInsert', 'DISPOSITIVO', 'DURACION', 'FECHA', 'FRANJA', 'GENERO', 'GRUPO_DE_MEDIOS', 'HORA',
+        'iddata_cpt', 'IMPRESIONES', 'INVERSION_DOLARES', 'INVERSION_LOCAL', 'LINK', 'MARCA', 'MEDIO', 'MES', 'PAIS',
+        'PRODUCTO', 'PROGRAMA', 'SECTOR', 'SPOTS', 'SUBTIPO_DE_MEDIO', 'TIPO', 'TIPO_MEDIO', 'TIPO_REPORTE', 'Total_AMAS', 'VERSION',
+        'Total_H18a60', 'Total_H18a60Al_Me', 'Total_H18a99Al_Me', 'Total_H25a39', 'Total_H25a60', 'Total_H25a60Al_Me',
+        'Total_H25a60Baja', 'Total_H40a60', 'Total_HyM12a24Al_Me', 'Total_HyM12a60', 'Total_HyM12a99', 'Total_HyM18a39',
+        'Total_HyM18a39Al_Me', 'Total_HyM18a39Baja', 'Total_HyM18a60', 'Total_HyM18a60Al_Me', 'Total_HyM18a60Baja',
+        'Total_HyM18a99', 'Total_HyM25a39', 'Total_HyM25a39Al_Me', 'Total_HyM25a39Baja', 'Total_HyM25a60',
+        'Total_HyM25a60Al_Me', 'Total_HyM25a99', 'Total_HyM25a99Al_Me', 'Total_HyM25a99Baja', 'Total_HyM3a99Total',
+        'Total_HyM40a60Al_Me', 'Total_M18a39', 'Total_M18a39Al_Me', 'Total_M18a39Baja', 'Total_M18a60', 'Total_M18a60Baja',
+        'Total_M25a39', 'Total_M25a60', 'Total_M25a60Baja']
 
-        # Agregar los encabezados de las columnas
-        # headers = ['AGENCIA', 'ANIO', 'ANUNCIANTE', 'AVISO', 'BIMESTRE', 'COLS', 'CADENA', 'CATEGORIA', 'CIRCULACION', 'CODIGO', 'COLOR', 'CORTE', 'DE_NPAGS', 'DISCO', 'DURACION', 'EST', 'EVENTO', 'FECHA', 'FRANJA', 'GENERO', 'HOLDING',
-        #            'LATITUD', 'LINK', 'LONGITUD', 'MARCA', 'MEDIO', 'MES', 'PLGS', 'PAIS', 'POSICION', 'PRODUCTO', 'PROGRAMA', 'SECTOR', 'SEMESTRE', 'SPOTS', 'SUBSECTOR', 'TIPODEMEDIO', 'TRIMESTRE', 'UNIDAD', 'VERSION', 'IDDATATL_DIARIO']
-       
-        headers = [    'ADSERVER', 'AGENCIA', 'ALTO', 'ANCHO', 'ANIO', 'ANUNCIANTE', 'AREA', 'AVISO', 'CAMPANIA', 'CATEGORIA', 
-    'CLIENTE', 'DateInsert', 'DISPOSITIVO', 'DURACION', 'FECHA', 'FRANJA', 'GENERO', 'GRUPO_DE_MEDIOS', 'HORA',
-    'iddata_cpt', 'IMPRESIONES', 'INVERSION_DOLARES', 'INVERSION_LOCAL', 'LINK', 'MARCA', 'MEDIO', 'MES', 'PAIS',
-    'PRODUCTO', 'PROGRAMA', 'SECTOR', 'SPOTS', 'SUBTIPO_DE_MEDIO', 'TIPO', 'TIPO_MEDIO', 'TIPO_REPORTE', 'Total_AMAS','VERSION',
-    'Total_H18a60', 'Total_H18a60Al_Me', 'Total_H18a99Al_Me', 'Total_H25a39', 'Total_H25a60', 'Total_H25a60Al_Me',
-    'Total_H25a60Baja', 'Total_H40a60', 'Total_HyM12a24Al_Me', 'Total_HyM12a60', 'Total_HyM12a99', 'Total_HyM18a39',
-    'Total_HyM18a39Al_Me', 'Total_HyM18a39Baja', 'Total_HyM18a60', 'Total_HyM18a60Al_Me', 'Total_HyM18a60Baja',
-    'Total_HyM18a99', 'Total_HyM25a39', 'Total_HyM25a39Al_Me', 'Total_HyM25a39Baja', 'Total_HyM25a60',
-    'Total_HyM25a60Al_Me', 'Total_HyM25a99', 'Total_HyM25a99Al_Me', 'Total_HyM25a99Baja', 'Total_HyM3a99Total',
-    'Total_HyM40a60Al_Me', 'Total_M18a39', 'Total_M18a39Al_Me', 'Total_M18a39Baja', 'Total_M18a60', 'Total_M18a60Baja',
-    'Total_M25a39', 'Total_M25a60', 'Total_M25a60Baja']
-
-        ws.append(headers)
-
-        # Agregar los resultados a las filas
-        for result in results:
-            row_data = [getattr(result, field, '') for field in headers]
-            ws.append(row_data)
-
-        # Guardar el archivo Excel en un objeto de BytesIO
-        from io import BytesIO
         excel_data = BytesIO()
-        wb.save(excel_data)
+        wb = xlsxwriter.Workbook(excel_data, {'in_memory': True})
+        ws = wb.add_worksheet()
+
+        # Agregar encabezados
+        for col, header in enumerate(headers):
+            ws.write(0, col, header)
+
+        for row, result in enumerate(results, start=1):
+            for col, field in enumerate(headers):
+                if field == 'FECHA' or field == "DateInsert":
+                    fecha = getattr(result, field, '')
+                    ws.write(row, col, str(fecha))
+                else:
+                    ws.write(row, col, getattr(result, field, ''))
+        wb.close()
+        del wb
         excel_data.seek(0)
         nombre_final = "competencia_" + \
             str(datetime.now().strftime("%Y-%m-%d"))
@@ -403,11 +415,10 @@ def generar_excel():
         response = make_response(
             send_file(excel_data, download_name=nombre_final+'.xlsx', as_attachment=True))
         response.status_code = 200
-
         return response
-
     return "No hay resultados", 404
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=7000)
+    #app.run(host='0.0.0.0', port=7000)
+    app.run(host='0.0.0.0', debug=True, port=7000)
